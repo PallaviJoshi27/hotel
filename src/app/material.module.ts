d8d0bb0fcbbab7a1bfe0286e901353a243b04b9d
vaibhavp-cuelogic <vaibhav.pathak@cuelogic.com>
2019-11-03 06:17:27 -0600

    added navigation components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import {MatBadgeModule} from '@angular/material/badge';
-import {MatButtonModule} from '@angular/material';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material';
 @NgModule({
-    imports : [MatBadgeModule , MatButtonModule],
-    exports : [MatBadgeModule, MatButtonModule]
+    imports : [MatBadgeModule , 
+                MatButtonModule,
+                MatToolbarModule,
+                MatIconModule,
+                MatSidenavModule,
+                MatListModule,
+                MatTabsModule],
+    exports : [MatBadgeModule, 
+                MatButtonModule,
+                MatToolbarModule,
+                MatIconModule,
+                MatSidenavModule,
+                MatListModule,
+                MatTabsModule]
 })
 
 export class MaterialModule{
